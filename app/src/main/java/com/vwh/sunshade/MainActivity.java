354d92cb10998e295491e61d9e6f701ac29fe022
Vincent Hugues <vincenthugues.dev@gmail.com>
2015-08-21 14:03:49 +0200

    Add latitude, longitude, and locality display

--- app/src/main/java/com/vwh/sunshade/MainActivity.java
+++ app/src/main/java/com/vwh/sunshade/MainActivity.java
@@ -2,24 +2,59 @@ package com.vwh.sunshade;
 
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import java.io.IOException;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buildGoogleApiClient();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(null, "Connection suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(null, "Connection failed");
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
     /*
@@ -46,36 +81,53 @@ public class MainActivity extends AppCompatActivity {
     }
     */
 
-    public void onCustomLocationEdittextClick(View view) {
-        /*
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
+    protected void displayInfo(double latitude, double longitude, String locality, String sunrise, String sunset) {
+        // Latitude
+        TextView latitudeDisplay = (TextView) findViewById(R.id.latitudeDisplayTextView);
+        latitudeDisplay.setText(String.valueOf(latitude));
 
-        // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.radio_pirates:
-                if (checked)
-                    // Pirates are the best
-                    break;
-            case R.id.radio_ninjas:
-                if (checked)
-                    // Ninjas rule
-                    break;
-        }
-        */
+        // Longitude
+        TextView longitudeDisplay = (TextView) findViewById(R.id.longitudeDisplayTextView);
+        longitudeDisplay.setText(String.valueOf(longitude));
 
-        Log.i(null, "Test");
+        // Locality
+        TextView localityDisplay = (TextView) findViewById(R.id.localityDisplayTextView);
+        localityDisplay.setText(locality);
 
-        EditText customLocationEditText = (EditText) view;
-        customLocationEditText.setFocusable(true);
-        customLocationEditText.setText("");
-        customLocationEditText.setActivated(true);
+        // Locality
+        TextView sunriseDisplay = (TextView) findViewById(R.id.sunriseDisplayTextView);
+        sunriseDisplay.setText(sunrise);
 
-        RadioButton customLocationRadioButton = (RadioButton) findViewById(R.id.custom_location_radiobutton);
-        customLocationRadioButton.setChecked(true);
+        // Locality
+        TextView sunsetDisplay = (TextView) findViewById(R.id.sunsetDisplayTextView);
+        sunsetDisplay.setText(sunset);
+
+        // Make info visible
+        LinearLayout infoLayout = (LinearLayout) findViewById(R.id.infoLayout);
+        infoLayout.setVisibility(View.VISIBLE);
     }
 
-    public void onGetInfoButtonClick(View view) {
+    protected void getCurrentLocationInfo() {
+        List<Address> addressList = null;
+        Geocoder geocoder = new Geocoder(this);
+
+        try {
+            if (mLastLocation != null) {
+                addressList = geocoder.getFromLocation(mLastLocation.getLatitude(), mLastLocation.getLongitude(), 1);
+            } else {
+                Log.i(null, "Location empty");
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        if (addressList != null && !addressList.isEmpty()) {
+            Address address = addressList.get(0);
+            displayInfo(address.getLatitude(), address.getLongitude(), address.getLocality(), "", "");
+        }
+    }
+
+    protected void getCustomLocationInfo() {
         EditText customLocationEditText = (EditText) findViewById(R.id.custom_location_edittext);
         String location = customLocationEditText.getText().toString();
         List<Address> addressList = null;
@@ -91,15 +143,25 @@ public class MainActivity extends AppCompatActivity {
             if (addressList != null && !addressList.isEmpty()) {
                 Address address = addressList.get(0);
                 //LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
-                Log.i(null, address.getLocality());
+                displayInfo(address.getLatitude(), address.getLongitude(), address.getLocality(), "", "");
             }
         }
+    }
 
-        LinearLayout sunriseLayout = (LinearLayout) findViewById(R.id.sunriseLayout);
-        sunriseLayout.setVisibility(View.VISIBLE);
+    public void onGetInfoButtonClick(View view) {
+        // Is the button now checked?
+        RadioGroup locationRadioGroup = (RadioGroup) findViewById(R.id.location_radiogroup);
+        int checkedRadioButtonId = locationRadioGroup.getCheckedRadioButtonId();
 
-        LinearLayout sunsetLayout = (LinearLayout) findViewById(R.id.sunsetLayout);
-        sunsetLayout.setVisibility(View.VISIBLE);
+        // Check which radio button was checked
+        switch(checkedRadioButtonId) {
+            case R.id.current_location_radiobutton:
+                getCurrentLocationInfo();
+                break;
+            case R.id.custom_location_radiobutton:
+                getCustomLocationInfo();
+                break;
+        }
     }
 
     public void onCurrentLocationRadioButtonCLick(View view) {
@@ -111,6 +173,7 @@ public class MainActivity extends AppCompatActivity {
     public void onCustomLocationRadioButtonCLick(View view) {
         EditText customLocationEditText = (EditText) findViewById(R.id.custom_location_edittext);
         customLocationEditText.setEnabled(true);
+        customLocationEditText.setText("");
         customLocationEditText.setVisibility(View.VISIBLE);
     }
 }
